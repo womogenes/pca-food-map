3fa7db2aaee78f11e8c71ef7c614d4d5d8e2f86f
William Y. Feng <womogenes2@gmail.com>
2022-08-05 16:37:00 -0400

    Fix first href

--- analysis/people_predictor.py
+++ analysis/people_predictor.py
@@ -2,7 +2,7 @@
 import pickle
 import numpy as np
 
-with open("./pca_models/people_2d_model.pkl", "rb") as fin:
+with open("./analysis/pca_models/people_2d_model.pkl", "rb") as fin:
     pca_2d = pickle.load(fin)
 
 
@@ -24,15 +24,19 @@ def get_point(spreadsheet_row):
     prefs = []
     parts = spreadsheet_row.split("\t")[3:-2][:50]
 
-    for part in parts:
-        prefs.append(np.nan if part == "" else float(part))
+    with open("./analysis/pca_models/mean_foods", "r") as f:
+        mean_vals = [float(i) for i in f.read().strip().split("\n")]
+
+    for i, part in enumerate(parts):
+        prefs.append(mean_vals[i] if part == "" else float(part))
 
     prefs = np.array(prefs)
     return transform(prefs)
+
 
 while True:
     col = input("\nRow: ")
     try:
         print(get_point(col))
-    except:
-        print("Error :(")
+    except Exception as e:
+        print(e)
